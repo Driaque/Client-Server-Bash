0a6def15eda7b163157cb5f508d42559e25ffb5d
Driaque <dan.onyeani@gmail.com>
2019-11-15 16:58:42 -0500

    first commit

---
+++ client.c
@@ -0,0 +1,83 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+
+
+/**
+*	This program is the client end of a remote shell through socket.
+*	The client program must run after the server program.
+*	Usage: <Program> <IP address> <Port>
+*/
+
+// maximum lenth of arguments which should be big enough to make sure the socket never fails of big amount of data
+#define MAX_LENGTH 1000000
+// maximum lenth of each command and each argument
+#define MAX_WORD_LENGTH 40
+
+char* myRead(char *s, int fd);
+
+
+int main(int argc, char* argv[]) {
+	char message[MAX_LENGTH];
+	int server, portNumber, pid, n, status;
+	socklen_t len;
+	struct sockaddr_in servAdd;
+
+	if (argc != 3) {
+		fprintf(stderr, "Call model:%s <IP> <Port>\n", argv[0]);
+		exit(0);
+	}
+
+	if ((server = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		fprintf(stderr, "Cannot create a socket\n");
+		exit(1);
+	}
+
+	//config the server socket
+	servAdd.sin_family = AF_INET;
+	sscanf(argv[2], "%d", &portNumber);
+	servAdd.sin_port = htons((uint16_t)portNumber);
+	if (inet_pton(AF_INET, argv[1], &servAdd.sin_addr) < 0) {
+		fprintf(stderr, " inet_pton() has failed\n");
+		exit(2);
+	}
+	//connect to the server
+	if (connect(server, (struct sockaddr*) &servAdd, sizeof(servAdd)) < 0) {
+		fprintf(stderr, "Connection refused\n");
+		exit(3);
+	}
+
+	while (1) {
+		pid = fork();
+		if (!pid) {
+			//prompt
+			write(STDOUT_FILENO, ">>", 2);
+			n = read(STDIN_FILENO, message, MAX_LENGTH);
+			message[n] = '\0';
+			//send commands to the server using space" " to seperate arguments
+			write(server, message, strlen(message) + 1);
+			//user types 'quit' to close the connection
+			if (!strcasecmp(message, "quit\n")) {
+				close(server);
+				kill(getppid(), SIGTERM);
+				exit(0);
+			}
+			if (n = read(server, message, MAX_LENGTH)) {
+				message[n] = '\0';
+				write(STDOUT_FILENO, message, n + 1);
+			}
+			exit(0);
+		}
+		else {
+			wait(&status);
+		}
+	}
+}
+
+
---
+++ server.c
@@ -0,0 +1,141 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+/**
+*	This program is the server end of a remote shell through socket.
+*	The client program must run firstly then the clients can connect to the machine.
+*	Usage: <Program> <Port>
+*/
+
+
+// maximum lenth of arguments which should be big enough to make sure the socket never fails of big amount of data
+#define MAX_LENGTH 1000000
+// maximum lenth of each command and each argument
+#define MAX_WORD_LENGTH 40
+
+void serverClient(int s);
+int mySplit(char** res, char* s, char* delim);
+
+int main(int argc, char* argv[]) {
+	int sd, client, portNumber, status;
+	socklen_t len;
+	struct sockaddr_in servAdd;
+
+	if (argc != 2) {
+		printf("Call model: %s <Port:#>\n", argv[0]);
+		exit(0);
+	}
+
+	if ((sd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("Wrong creating new socket");
+		exit(1);
+	}
+
+	//config the server socket
+	servAdd.sin_family = AF_INET;
+	servAdd.sin_addr.s_addr = htonl(INADDR_ANY);
+	sscanf(argv[1], "%d", &portNumber);
+	servAdd.sin_port = htons((uint16_t)portNumber);
+	bind(sd, (struct sockaddr*) &servAdd, sizeof(servAdd));
+
+	// start listening to the assigned port for at most 5 clients
+	listen(sd, 5);
+	while (1) {
+		// wait for clients from any IP address
+		client = accept(sd, (struct sockaddr*) NULL, NULL);
+		printf("Got a client\n");
+		// fork a process for each client
+		if (!fork())
+			serverClient(client);
+		close(client);
+		waitpid(0, &status, WNOHANG);
+	}
+
+}
+
+
+
+/**
+* int mySplit(char** res, char *s, char* delim);
+* implement custom Split function to extract command and arguments from the given string.
+* parameters:
+*      char** res: point of a array of string where the array stores commands
+*      and arguments.
+*      char* s: point of the given string.
+*      char* delim: point of the string as a delimiter.
+*  return value:
+*      return the number of command plus arguments.
+*/
+int mySplit(char** res, char* s, char* delim) {
+	//change the last character of s from '\n' to '\0' otherwise execvp()
+	//cannot be executed.
+	s[strlen(s) - 1] = 0;
+	char * token;
+	token = strtok(s, delim);
+	int i = 0;
+	// use while loop to seperate string s iteratively.
+	while (token != NULL) {
+		res[i] = (char*)malloc(sizeof(char) * strlen(token));
+		strcpy(res[i++], token);
+		token = strtok(NULL, delim);
+	}
+	//return size of the array.
+	return i;
+}
+
+
+/**
+*	void serverClient(int sd);
+*	function to deal with a client
+*	parameters:
+*		int sd: socket descriptor returned by socket().
+*/
+void serverClient(int sd) {
+	char message[MAX_LENGTH];
+	int n, pid, status;
+	char **res;
+
+	//make the screen descriptor designate the client socket
+	dup2(sd, STDOUT_FILENO);
+	while (1) {
+		pid = fork();
+		// fork a new process for every command
+		if (!pid) {
+			//read from client
+			if (n = read(sd, message, MAX_LENGTH)) {
+				// quit if the client sends 'quit'
+				message[n] = '\0';
+				if (!strcasecmp(message, "quit\n")) {
+					fprintf(stderr, "%s\n", "Client disconnected, waiting for a new client...");
+					close(sd);
+					kill(getppid(), SIGTERM);
+					exit(0);
+				}
+				//split commands with space" "
+				res = (char**)malloc(sizeof(char*) * MAX_WORD_LENGTH);
+				int num = mySplit(res, message, " ");
+
+				int i;
+				// execute commands and output it to the client
+				if (execvp(res[0], res) == -1) {
+					printf("Error in executing the command\n");
+				}
+				// free the memory allocated before
+				for (i = 0; i < num; i++) {
+					free(res[i]);
+				}
+				free(res);
+				exit(0);
+			}
+		}
+		else {
+			wait(&status);
+		}
+	}
+}
